d68f5eb7765cb71902c9bd56aba57176832f3d6e
Reem-ELardy <136978871+Reem-ELardy@users.noreply.github.com>
2026-01-17 03:23:20 +0200

    Register User test cases and test data

---
+++ src/test/java/pom/tests/UiTesting/RegisterUserTest.java
@@ -0,0 +1,81 @@
+package pom.tests.UiTesting;
+
+import io.qameta.allure.*;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import utils.Framework.DriverFactory;
+import utils.Framework.JsonFileReader;
+import utils.Framework.TestNgListener;
+import utils.HelperFunctions.UserFunctions;
+import pom.pages.*;
+
+
+@Feature("User SignUp")
+@Listeners({TestNgListener.class})
+public class RegisterUserTest {
+    JsonFileReader testDataManager;
+
+    @Test(description = "Register a new User")
+    @Description("End-to-end sign up process for a new user")
+    @Severity(SeverityLevel.CRITICAL)
+    @TmsLink("AT-12")
+    public void TC1_testRegisterUser() {
+        new HomePage()
+                .assertHomePageVisible();
+        new Dashboard()
+                .clickSignupLoginButton();
+        new SignUpLoginPage()
+                .assertSignupFormTitle(testDataManager.getData("AssertionData.SignUpFormTitle"))
+                .SignUp(testDataManager.getData("userTestData.username"), testDataManager.getData("userTestData.email"));
+        new SignUpPage()
+                .assertOnEnterAccountInformationTitle(testDataManager.getData("AssertionData.EnterAccountInfoTitle"))
+                .fillAccountInformation(testDataManager.getData("userTestData.title"), testDataManager.getData("userTestData.password"), testDataManager.getData("userTestData.day"), testDataManager.getData("userTestData.month"), testDataManager.getData("userTestData.year"))
+                .fillAddressPersonalInformation(testDataManager.getData("userTestData.firstName"), testDataManager.getData("userTestData.lastName"), testDataManager.getData("userTestData.company"), testDataManager.getData("userTestData.mobileNumber"))
+                .fillAddressLocationInformation(testDataManager.getData("userTestData.address"), testDataManager.getData("userTestData.address2"), testDataManager.getData("userTestData.country"), testDataManager.getData("userTestData.state"), testDataManager.getData("userTestData.city"), testDataManager.getData("userTestData.zipCode"))
+                .clickOnCreateAccountButton();
+        new AccountCreatedPage()
+                .assertAccountCreatedPageTitle(testDataManager.getData("AssertionData.AccountCreatedPageTitle"))
+                .clickContinueButton();
+        new Dashboard()
+                .assertOnLoggedInAs(testDataManager.getData("userTestData.username"));
+
+    }
+
+    @Test
+    @TmsLink("AT-16")
+    public void TC5_RegisterExistingUser() {
+        new HomePage()
+                .assertHomePageVisible();
+        new Dashboard()
+                .clickSignupLoginButton();
+        new SignUpLoginPage()
+                .assertSignupFormTitle(testDataManager.getData("AssertionData.SignUpFormTitle"))
+                .SignUp(testDataManager.getData("userTestData2.username"), testDataManager.getData("userTestData2.email"))
+                .assertOnSignUpError(testDataManager.getData("AssertionData.SignUpFormError"));
+
+        UserFunctions.SignInUser(testDataManager,  "userTestData2");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        UserFunctions.deleteUserAccount();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        new HomePage()
+                .navigateToHomePage();
+    }
+
+    @BeforeClass(description = "SetUp browser, json file reader")
+    public void setUp() {
+        DriverFactory.initiateDriver();
+        testDataManager = new JsonFileReader("UITestingData/registerTestData.json");
+        UserFunctions.SignUpAndLogout(testDataManager, "userTestData2");
+    }
+
+    @AfterClass(description = "Delete the created test account and close the browser after the test suite")
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+}
