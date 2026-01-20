3ef4d522c1bb29007fa446c3142738969f6a4c59
Reem-ELardy <136978871+Reem-ELardy@users.noreply.github.com>
2026-01-20 22:47:00 +0200

    Adding Other buttons of Dashboard

--- src/main/java/pom/pages/Dashboard.java
+++ src/main/java/pom/pages/Dashboard.java
@@ -11,25 +11,47 @@ public class Dashboard {
     //Variables
 
     //Locators
-    private By signupLoginButton = By.xpath("//a[@href=\"/login\"]");
+    private By HomeButton = By.xpath("//a[@href='/']");
+    private By productsButton = By.xpath("//a[@href='/products']");
+    private By cartButton = By.xpath("//a[@href='/view_cart']");
+    private By signupLoginButton = By.xpath("//a[@href='/login']");
+    private By deleteAccountButton = By.xpath("//a[@href='/delete_account']");
+    private By logOutButton = By.xpath("//a[@href='/logout']");
+    private By testCasesButton = By.xpath("//a[@href='/test_cases']");
+    private By apiTestingButton =  By.xpath("//a[@href='/api_list']");
+    private By videoTutorialsButton = By.xpath("//a[contains(@href,'AutomationExercise')]");
+    private By contactUsButton = By.xpath("//a[@href='/contact_us']");
     private By loggedInAsText = By.xpath("//i[@class = 'fa fa-user']/parent::a");
-    private By deleteAccountButton = By.xpath("//a[@href=\"/delete_account\"]");
-    private By logOutButton = By.xpath("//a[@href=\"/logout\"]");
-
 
     //Constructor
 
     //Actions
-    @Step("Click On Signup Login")
-    public void clickSignupLoginButton() {
+    @Step("Click On Home")
+    public Dashboard clickHomeButton() {
         AdvertismentPages.closeAdsIfAny();
-        ElementActions.Click(signupLoginButton);
+        ElementActions.Click(HomeButton);
+        return this;
     }
 
-    @Step("Click On Delete Account")
-    public void clickDeleteAccountButton() {
+    @Step("Click On Product")
+    public Dashboard clickProductsButton() {
         AdvertismentPages.closeAdsIfAny();
-        ElementActions.Click(deleteAccountButton);
+        ElementActions.Click(productsButton);
+        return this;
+    }
+
+    @Step("Click On Cart")
+    public Dashboard clickCartButton() {
+        AdvertismentPages.closeAdsIfAny();
+        ElementActions.Click(cartButton);
+        return this;
+    }
+
+    @Step("Click On Signup Login")
+    public Dashboard clickSignupLoginButton() {
+        AdvertismentPages.closeAdsIfAny();
+        ElementActions.Click(signupLoginButton);
+        return this;
     }
 
     @Step("Click On Logout")
@@ -39,11 +61,56 @@ public class Dashboard {
         return this;
     }
 
+    @Step("Click On Delete Account")
+    public Dashboard clickDeleteAccountButton() {
+        AdvertismentPages.closeAdsIfAny();
+        ElementActions.Click(deleteAccountButton);
+        return this;
+    }
+
+
+    @Step("Click On Test Cases")
+    public Dashboard clickTestCasesButton() {
+        AdvertismentPages.closeAdsIfAny();
+        ElementActions.Click(testCasesButton);
+        return this;
+    }
+
+    @Step("Click On API Testing")
+    public Dashboard clickApiTestingButton() {
+        AdvertismentPages.closeAdsIfAny();
+        ElementActions.Click(apiTestingButton);
+        return this;
+    }
+
+    @Step("Click On VideoTutorials")
+    public Dashboard clickVideoTutorialsButton() {
+        AdvertismentPages.closeAdsIfAny();
+        ElementActions.Click(videoTutorialsButton);
+        return this;
+    }
+
+    @Step("Click On Contact Us")
+    public Dashboard clickContactUsButton() {
+        AdvertismentPages.closeAdsIfAny();
+        ElementActions.Click(contactUsButton);
+        return this;
+    }
+
     //Validation
     @Step("Assert On Logged In As Username")
     public Dashboard assertOnLoggedInAs(String username) {
         AdvertismentPages.closeAdsIfAny();
         Assert.assertEquals(ElementActions.getText(loggedInAsText), "Logged in as " + username);
+        return this;
+    }
+
+    public Dashboard isNavigatedToYouTube() {
+        AdvertismentPages.closeAdsIfAny();
+        String ExpectedUrl = "https://www.youtube.com/";
+        String actualUrl = DriverFactory.getDriver().getCurrentUrl();
+
+        Assert.assertTrue(actualUrl.contains(ExpectedUrl));
         return this;
     }
 }
