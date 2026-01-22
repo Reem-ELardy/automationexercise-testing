ec36f9c5332509e54e46c203f3feec00e09fae6a
Reem-ELardy <136978871+Reem-ELardy@users.noreply.github.com>
2026-01-22 03:54:56 +0200

    Add closing sticky ads

--- src/main/java/pom/pages/AdvertismentPages.java
+++ src/main/java/pom/pages/AdvertismentPages.java
@@ -1,14 +1,13 @@
 package pom.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import utils.Framework.DriverFactory;
 import utils.Framework.ElementActions;
 
 public class AdvertismentPages {
 
-    private static By close = By.id("dismiss-button");
     private static By dismiss = By.id("dismiss-button");
     private static By adDiv = By.id("ad_position_box");
 
@@ -16,17 +15,25 @@ public class AdvertismentPages {
         WebDriver driver = DriverFactory.getDriver();
         try {
             // Use try/catch to avoid Timeout if element not present
-            if (ElementActions.isElementPresent(close) || ElementActions.isElementPresent(dismiss)) {
-                try {
-                    ElementActions.Click(close);
-                } catch (NoSuchElementException e) {
-                    ElementActions.Click(dismiss);
-                }
+            if (ElementActions.isElementPresent(dismiss)) {
+                ElementActions.Click(dismiss);
                 driver.switchTo().defaultContent(); // return to main content
                 System.out.println("Ad closed successfully.");
             }
         } catch (Exception e) {
             System.out.println("No ad to close or error occurred: " + e.getMessage());
         }
+    }
+
+    public static void closeStickyAds() {
+        JavascriptExecutor js = (JavascriptExecutor) DriverFactory.getDriver();
+
+        // Hide all ads with class 'adsbygoogle'
+        js.executeScript(
+                "var ads = document.querySelectorAll('ins.adsbygoogle');" +
+                        "for(var i=0; i<ads.length; i++){" +
+                        "   ads[i].style.display='none';" +
+                        "}"
+        );
     }
 }
