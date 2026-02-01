253584fd5fc07251c6beb3e7fe08577fea25b9a2
Reem-ELardy <136978871+Reem-ELardy@users.noreply.github.com>
2026-02-01 19:22:22 +0200

    Edits in naming

--- src/test/java/pom/tests/APITesting/DeleteUserAPITests.java
+++ src/test/java/pom/tests/APITesting/DeleteUserAPITests.java
@@ -20,7 +20,7 @@ public class DeleteUserAPITests {
     @TmsLink("AT-61")
     @Description("Verify that DELETE /deleteAccount with valid email and password returns 200 and 'Account deleted!'")
     @Severity(SeverityLevel.CRITICAL)
-    public void API17_DeleteUserAccountWithValidCredentials() {
+    public void API18_DeleteUserAccountWithValidCredentials() {
         Response response = APICallsUtils.deleteRequest(APIEndpoints.DELETE_ACCOUNT, apiTestDataManager.getTestDataMap("UserData"));
         Assert.assertEquals(response.jsonPath().getInt("responseCode"), 200);
         Assert.assertEquals(response.jsonPath().getString("message"), "Account deleted!");
@@ -30,7 +30,7 @@ public class DeleteUserAPITests {
     @TmsLink("AT-69")
     @Description("Verify that DELETE /deleteAccount with invalid email or password returns 404 and 'Account not found!'")
     @Severity(SeverityLevel.CRITICAL)
-    public void API18_DeleteUserAccountWithInvalidCredentials() {
+    public void API19_DeleteUserAccountWithInvalidCredentials() {
         Response response = APICallsUtils.deleteRequest(APIEndpoints.DELETE_ACCOUNT, apiTestDataManager.getTestDataMap("UserDataInvalidPassword"));
         Assert.assertEquals(response.jsonPath().getInt("responseCode"), 404);
         Assert.assertEquals(response.jsonPath().getString("message"), "Account not found!");
@@ -40,7 +40,7 @@ public class DeleteUserAPITests {
     @TmsLink("AT-70")
     @Description("Verify that DELETE /deleteAccount without providing the email parameter returns 400 and a validation error message")
     @Severity(SeverityLevel.CRITICAL)
-    public void API19_DeleteUserAccountWithMissingEmail() {
+    public void API20_DeleteUserAccountWithMissingEmail() {
         Response response = APICallsUtils.deleteRequest(APIEndpoints.DELETE_ACCOUNT, apiTestDataManager.getTestDataMap("UserDataWithMissingEmail"));
         Assert.assertEquals(response.jsonPath().getInt("responseCode"), 400);
         Assert.assertEquals(response.jsonPath().getString("message"), "Bad request, email parameter is missing in DELETE request.");
