1c5b4dc5a38aebdbcd397fc362409d6a22186b77
Reem-ELardy <136978871+Reem-ELardy@users.noreply.github.com>
2026-02-01 19:47:28 +0200

    REGISTER_TO_GET_USER_FIELD_MAPPING

--- src/main/java/utils/HelperClasses/APIEndpoints.java
+++ src/main/java/utils/HelperClasses/APIEndpoints.java
@@ -1,5 +1,7 @@
 package utils.HelperClasses;
 
+import java.util.Map;
+
 public class APIEndpoints {
 
     // Product APIs
@@ -15,4 +17,22 @@ public class APIEndpoints {
     public static final String UPDATE_ACCOUNT = "/updateAccount";
     public static final String DELETE_ACCOUNT = "/deleteAccount";
     public static final String GET_USER_BY_EMAIL = "/getUserDetailByEmail";
+
+    public static final Map<String, String> REGISTER_TO_GET_USER_FIELD_MAPPING = Map.ofEntries(
+            Map.entry("name", "name"),
+            Map.entry("email", "email"),
+            Map.entry("title", "title"),
+            Map.entry("birth_day", "birth_date"),
+            Map.entry("birth_month", "birth_month"),
+            Map.entry("birth_year", "birth_year"),
+            Map.entry("first_name", "firstname"),
+            Map.entry("last_name", "lastname"),
+            Map.entry("company", "company"),
+            Map.entry("address1", "address1"),
+            Map.entry("address2", "address2"),
+            Map.entry("country", "country"),
+            Map.entry("state", "state"),
+            Map.entry("city", "city"),
+            Map.entry("zipcode", "zipcode")
+    );
 }
