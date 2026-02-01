779bf03512f7a630867bbd118753b2aa5f350ea8
Reem-ELardy <136978871+Reem-ELardy@users.noreply.github.com>
2026-02-01 17:56:16 +0200

    Login API tests

---
+++ src/test/java/pom/tests/APITesting/LoginUserAPITests.java
@@ -0,0 +1,71 @@
+package pom.tests.APITesting;
+
+import io.qameta.allure.*;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import utils.Framework.APICallsUtils;
+import utils.Framework.JsonFileReader;
+import utils.Framework.TestNgListener;
+import utils.HelperClasses.APIEndpoints;
+
+@Feature("Login User API")
+@Listeners({TestNgListener.class})
+public class LoginUserAPITests {
+    JsonFileReader apiTestDataManager;
+
+    @Test
+    @TmsLink("AT-54")
+    @Description("Verify that POST /verifyLogin with valid email and password returns 200 and 'User exists!' message")
+    @Severity(SeverityLevel.CRITICAL)
+    public void API10_PostToVerifyLoginWithValidDetails() {
+        Response response = APICallsUtils.postRequest(APIEndpoints.VERIFY_LOGIN, apiTestDataManager.getTestDataMap("LoginWithValidData"));
+        Assert.assertEquals(response.jsonPath().getInt("responseCode"), 200);
+        Assert.assertEquals(response.jsonPath().getString("message"), "User exists!");
+    }
+
+    @Test
+    @TmsLink("AT-57")
+    @Description("Verify that POST /verifyLogin with invalid credentials returns 404 'User not found!'")
+    @Severity(SeverityLevel.CRITICAL)
+    public void API11_PostToVerifyLoginWithInvalidDetails() {
+        Response response = APICallsUtils.postRequest(APIEndpoints.VERIFY_LOGIN, apiTestDataManager.getTestDataMap("LoginWithInvalidData"));
+        Assert.assertEquals(response.jsonPath().getInt("responseCode"), 404);
+        Assert.assertEquals(response.jsonPath().getString("message"), "User not found!");
+    }
+
+    @Test
+    @TmsLink("AT-55")
+    @Description("Verify that POST /verifyLogin without email parameter returns 400 Bad Request")
+    @Severity(SeverityLevel.CRITICAL)
+    public void API12_PostToVerifyLoginWithoutEmailParameter() {
+        Response response = APICallsUtils.postRequest(APIEndpoints.VERIFY_LOGIN, apiTestDataManager.getTestDataMap("LoginWithMissingEmail"));
+        Assert.assertEquals(response.jsonPath().getInt("responseCode"), 400);
+        Assert.assertEquals(response.jsonPath().getString("message"), "Bad request, email or password parameter is missing in POST request.");
+    }
+
+    @Test
+    @TmsLink("AT-56")
+    @Description("Verify that DELETE /verifyLogin returns 405 Method Not Allowed")
+    @Severity(SeverityLevel.NORMAL)
+    public void API13_DeleteToVerifyLogin() {
+        Response response = APICallsUtils.deleteRequest(APIEndpoints.VERIFY_LOGIN);
+        Assert.assertEquals(response.jsonPath().getInt("responseCode"), 405);
+        Assert.assertEquals(response.jsonPath().getString("message"), "This request method is not supported.");
+    }
+
+    @BeforeClass(description = "SetUp json file reader, Create User Account")
+    public void setUp() {
+        apiTestDataManager = new JsonFileReader("APITestingData/LoginUserTestData.json");
+        APICallsUtils.postRequest(APIEndpoints.CREATE_ACCOUNT, apiTestDataManager.getTestDataMap("RegisterFirstUserData"));
+    }
+
+    @AfterClass(description = "Delete Created user Account")
+    public void tearDown() {
+        APICallsUtils.deleteRequest(APIEndpoints.DELETE_ACCOUNT, apiTestDataManager.getTestDataMap("LoginWithValidData"));
+    }
+
+}
